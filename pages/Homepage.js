9b21731ffc077acbc94bb7b3d54dc052859452d5
Apoorv Sharma <apoorvsharma494@gmail.com>
2023-10-26 20:35:20 +0530

    commit

--- pages/Homepage.js
+++ pages/Homepage.js
@@ -1,27 +1,26 @@
 const displayData = (element) =>{
-// Create a new swiper-slide div element
+
 const swiperSlide = document.createElement('div');
 swiperSlide.className = 'swiper-slide';
 const swiperMain = document.querySelector(".swiper-wrapper")
 
-// Create an img element
+
 const img = document.createElement('img');
 img.src = element.image;
 img.alt = '';
 
-// Create an h2 element for Brand
+
 const brandH2 = document.createElement('h2');
 brandH2.textContent = 'Brand : Nike';
 
-// Create a p element for the product name
+
 const productNameP = document.createElement('p');
 productNameP.textContent = element.name;
 
-// Create a div element for ratings
+
 const ratingsDiv = document.createElement('div');
 ratingsDiv.className = 'ratings';
 
-// Create five i elements for stars
 for (let i = 0; i < 3; i++) {
     const solidStar = document.createElement('i');
     solidStar.className = 'fa-solid fa-star';
@@ -34,16 +33,12 @@ for (let i = 0; i < 2; i++) {
     ratingsDiv.appendChild(regularStar);
 }
 
-// Create a button element for "Check out details"
-// const sliderBtn = document.createElement('button');
-// sliderBtn.className = 'slider-btn';
-// sliderBtn.textContent = 'Check out details';
+
 
 const viewDetailsLink = document.createElement('a');
   viewDetailsLink.href = `../pages/singleProducts/product-${element.id}.html`;
   viewDetailsLink.textContent = 'View Details';
 
-// Append all elements to the swiperSlide div
 swiperSlide.appendChild(img);
 swiperSlide.appendChild(brandH2);
 swiperSlide.appendChild(productNameP);
@@ -71,18 +66,4 @@ fetch('Homepage.json')
   });
 
 
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+ 
